7c83d356f10493a74fb1378df5da74cbd6c0dec7
Евгений Кислов <evgeniy@iMac-Evgenij.local>
2018-07-27 13:50:25 +0300

    improved jumps

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,40 +4,19 @@ public class PlayerControl : MonoBehaviour {
 
     public Animator animator;
     public Rigidbody2D rigidbody;
-    public LayerMask groundMask;
-
-    //public Rigidbody2D bullet;
-    //public GameObject scoper;
 
     private float jumpDirection;
     private float direction;
 
-    public float playerHeight;
-    public float playerWidth;
+    private float playerHeight;
+    private float playerWidth;
 
     private int speed = 20;
-    //private int jumpSpeed = 100;
+    private int jumpForce = 8000;
 
     private bool isFacingRight = true;
     private bool attacks;
-
-    private int jumpForce = 2000;
-
-    public bool isGrounded;
-
-    public float dist;
-
-    //private float scoperLen = 3;
-    //private float scoperAng = 0;
-    //private float scopeRotSpeed = 1;
-    //private float scopeMoveSpeed = 0.1f;
-    //private float scopeMaxLen = 50;
-    //private float fireForce = 100000;
-    //private const float angleRealtion = 0.0174533f;
-
-    //private Rigidbody2D curSlot, slot1, slot2, slot3;
-    //public Rigidbody2D weapon1, weapon2, weapon3;
-    //public Rigidbody2D scope;
+    private bool isGrounded;
 
     private Vector2 moveVector = new Vector2();
 
@@ -57,17 +36,10 @@ public class PlayerControl : MonoBehaviour {
 
     private void Start() {
         playerHeight = GetComponent<SpriteRenderer>().size.y * transform.localScale.y;
-
-        //scoper = Instantiate(scope.gameObject, new Vector2(0, 0), Quaternion.identity);
-        //slot1 = weapon1; slot2 = weapon2; slot3 = weapon3;
-        //curSlot = slot1;
     }
 
     void FixedUpdate() {
-        checkGrounded();
-
-        playerWidth =  System.Math.Abs(GetComponent<SpriteRenderer>().bounds.size.x / transform.lossyScale.x);
-        //GetComponent<BoxCollider2D>().size = new Vector2(playerWidth, GetComponent<BoxCollider2D>().size.y);
+        playerWidth = System.Math.Abs(GetComponent<SpriteRenderer>().bounds.size.x / transform.lossyScale.x);
 
         handleInput();
         handleState();
@@ -75,7 +47,6 @@ public class PlayerControl : MonoBehaviour {
 
     private void handleInput() {
         handleMoveInput();
-        //handleWeaponInput();
     }
 
     private void handleState() {
@@ -86,10 +57,8 @@ public class PlayerControl : MonoBehaviour {
         handleAdditionalState();
         handleBaseState();
 
-        moveVector.y = rigidbody.velocity.y;
+        //moveVector.y = rigidbody.velocity.y;
         rigidbody.velocity = moveVector;
-        //if (nearTheWall() && !isGrounded())
-            //rigidbody.AddForce(Vector2.down * 1000);
     }
 
     private void handleAdditionalState() {
@@ -124,34 +93,19 @@ public class PlayerControl : MonoBehaviour {
         attacks = false;
     }
 
-    void checkGrounded()
+    void OnTriggerStay2D(Collider2D other)
     {
-
-        //DEBUG
-        Debug.DrawRay(rigidbody.position - new Vector2(1.0f, 2.17f), Vector2.right * 2.0f, Color.green);
-
-        Debug.DrawRay(rigidbody.position + new Vector2(playerWidth, playerHeight) / 2.0f, Vector2.down * playerHeight, Color.green);
-        Debug.DrawRay(rigidbody.position + new Vector2(-playerWidth, playerHeight) / 2.0f, Vector2.down * playerHeight, Color.green);
-
-        bool hit_b = Physics2D.Raycast(rigidbody.position - new Vector2(1.0f, 2.17f), Vector2.right * 2.0f, groundMask);
-        //RaycastHit2D hit_r = Physics2D.Raycast(rigidbody.position + new Vector2(playerWidth, playerHeight) / 2.0f,
-        //                                       Vector2.down * playerHeight, groundMask);
-        //RaycastHit2D hit_l = Physics2D.Raycast(rigidbody.position + new Vector2(-playerWidth, playerHeight) / 2.0f,
-                                               //Vector2.down * playerHeight, groundMask);
-
-        isGrounded = hit_b;
+        Debug.Log("Stay");
+        if (other.gameObject.tag == "Ground")
+            isGrounded = true;     
     }
 
-    //bool nearTheWall()
-    //{
-    //    RaycastHit2D hit_r = Physics2D.Raycast(rigidbody.position, Vector2.right, playerWidth / 2.0f, groundMask);
-    //    RaycastHit2D hit_l = Physics2D.Raycast(rigidbody.position, Vector2.left, playerWidth / 2.0f, groundMask);
-
-    //    if ((hit_l.collider != null) || (hit_r.collider != null))
-    //        return true;
-        
-    //    return false;
-    //}
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Exit");
+        if (other.gameObject.tag == "Ground")
+        isGrounded = false;
+    }
 
     void handleMoveInput()
     {
@@ -167,37 +121,4 @@ public class PlayerControl : MonoBehaviour {
         else if (isGrounded)
             additionalState = AdditionalState.NONE;
     }
-
-    //void handleWeaponInput()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.F))
-    //        attacks = true;
-
-    //    if (isFacingRight)
-    //        scoper.transform.position = new Vector2(this.rigidbody.transform.position.x + scoperLen * Mathf.Cos(scoperAng * angleRealtion), this.rigidbody.transform.position.y + scoperLen * Mathf.Sin(scoperAng * angleRealtion));
-    //    if (!isFacingRight)
-    //        scoper.transform.position = new Vector2(rigidbody.transform.position.x - scoperLen * Mathf.Cos(scoperAng * angleRealtion), rigidbody.transform.position.y + scoperLen * Mathf.Sin(scoperAng * angleRealtion));
-    //    if ((Input.GetKey(KeyCode.Q) && (scoperAng < 90)))
-    //        scoperAng+=scopeRotSpeed;
-    //    if ((Input.GetKey(KeyCode.E) && (scoperAng > -90)))
-    //        scoperAng-=scopeRotSpeed;
-    //    if ((Input.GetKey(KeyCode.LeftBracket) && (scoperLen < scopeMaxLen)))
-    //        scoperLen += scopeMoveSpeed;
-    //    if ((Input.GetKey(KeyCode.RightBracket) && (scoperLen > 1)))
-    //        scoperLen -= scopeMoveSpeed;
-
-
-    //    if (Input.GetKeyDown(KeyCode.Alpha1))
-    //        curSlot = slot1;
-    //    if (Input.GetKeyDown(KeyCode.Alpha2))
-    //        curSlot = slot2;
-    //    if (Input.GetKeyDown(KeyCode.Alpha3))
-    //        curSlot = slot3;
-
-    //    int bulletDirection = isFacingRight ? 1 : -1;
-
-    //    if (Input.GetKeyDown(KeyCode.G))
-    //        Instantiate(curSlot, new Vector2(this.transform.position.x + bulletDirection * playerHeight/2.0f, this.transform.position.y), Quaternion.identity)
-    //            .AddForce((Vector2.up * Mathf.Sin(scoperAng * angleRealtion) + bulletDirection * Vector2.right * Mathf.Cos(scoperAng * angleRealtion)) * fireForce);
-    //}
 }
