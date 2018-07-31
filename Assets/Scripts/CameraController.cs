88735b4fcbb1ded65e4c9c57dc66eb1bbfb2e52c
Евгений Кислов <evgeniy@iMac-Evgenij.local>
2018-07-31 11:42:45 +0300

    camera bag fixed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraControl : MonoBehaviour {
+public class CameraController : MonoBehaviour {
 
     public GameObject player;
 
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemy : MonoBehaviour {
+public class EnemyController : MonoBehaviour {
     public Rigidbody2D enemyO;
      public int hp;
 
